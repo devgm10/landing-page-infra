5932de4e65694024d72040f3e438dfec8477ae68
Gianmarco Linares Becerra <devgm1995@outlook.com>
2026-07-02 01:21:58 -0500

    feat: compute module with EC2, Elastic IP and Docker

---
+++ modules/compute/main.tf
@@ -0,0 +1,34 @@
+data "aws_ami" "ubuntu" {
+    most_recent = true
+    owners      = ["099720109477"] # Canonical
+
+    filter {
+        name   = "name"
+        values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+    }
+    filter {
+        name   = "virtualization-type"
+        values = ["hvm"]
+    }
+}
+
+resource "aws_key_pair" "deployer" {
+    key_name   = "${var.project_name}-key"
+    public_key = var.ssh_public_key
+}
+
+resource "aws_instance" "web" {
+    ami                    = data.aws_ami.ubuntu.id
+    instance_type          = var.instance_type
+    key_name               = aws_key_pair.deployer.key_name
+    vpc_security_group_ids  = [var.security_group_id]
+    user_data              = file("${path.module}/user_data.sh")
+
+    tags = { Name = "${var.project_name}-server" }
+}
+
+resource "aws_eip" "web" {
+    instance = aws_instance.web.id
+    domain   = "vpc"
+    tags     = { Name = "${var.project_name}-eip" }
+}
---
+++ modules/compute/variables.tf
@@ -0,0 +1,15 @@
+variable "project_name" {
+    type = string
+}
+
+variable "instance_type" {
+    type = string
+}
+
+variable "ssh_public_key" {
+    type = string
+}
+
+variable "security_group_id" {
+    type = string
+}
