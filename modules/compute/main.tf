d95c099885121329c140324f66594cedb17077c3
Gianmarco Linares Becerra <devgm1995@outlook.com>
2026-07-04 03:33:01 -0500

    feat: enable EC2 management via SSM with IAM role and instance profile

--- modules/compute/main.tf
+++ modules/compute/main.tf
@@ -22,8 +22,8 @@ resource "aws_instance" "web" {
     instance_type          = var.instance_type
     key_name               = aws_key_pair.deployer.key_name
     vpc_security_group_ids  = [var.security_group_id]
+    iam_instance_profile   = aws_iam_instance_profile.ssm.name
     user_data              = file("${path.module}/user_data.sh")
-
     tags = { Name = "${var.project_name}-server" }
 }
 
@@ -31,4 +31,27 @@ resource "aws_eip" "web" {
     instance = aws_instance.web.id
     domain   = "vpc"
     tags     = { Name = "${var.project_name}-eip" }
+}
+
+resource "aws_iam_role" "ssm" {
+    name = "${var.project_name}-ssm-role"
+
+    assume_role_policy = jsonencode({
+        Version = "2012-10-17"
+        Statement = [{
+            Action    = "sts:AssumeRole"
+            Effect    = "Allow"
+            Principal = { Service = "ec2.amazonaws.com" }
+        }]
+    })
+}
+
+resource "aws_iam_role_policy_attachment" "ssm" {
+    role       = aws_iam_role.ssm.name
+    policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+resource "aws_iam_instance_profile" "ssm" {
+    name = "${var.project_name}-ssm-profile"
+    role = aws_iam_role.ssm.name
 }
